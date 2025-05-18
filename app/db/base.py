adba2ad7ae0e8c1b9f2e8d1da8d4583d6f876a82
ryu-min-ji <rm3319@naver.com>
2025-05-18 23:52:45 +0900

    base.py

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,5 @@
-from sqlalchemy.ext.declarative import declarative_base
+#Base 클래스 정의 파일
 
-Base = declarative_base()
+from sqlalchemy.ext.declarative import declarative_base # SQLAlchemy 모델 생성을 위한 기본 클래스 생성 함수
+
+Base = declarative_base() # 모든 모델들이 상속받는 공통 부모 클래스
