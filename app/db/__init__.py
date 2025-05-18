a1d50c85a36afb0eafafae90437e6d76750159b4
ryu-min-ji <rm3319@naver.com>
2025-05-18 23:51:09 +0900

    __init__.py

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,4 +1,5 @@
 # app/db/__init__.py
-from .base import Base
-from .session import get_db
+#import 할 수 있게 해주는 초기화 파일
 
+from .base import Base # Base: SQLAlchemy 모델 정의 시 사용하는 공통 부모 클래스 (declarative_base())
+from .session import get_db # get_db: FastAPI 의존성 주입용 DB 세션 생성 함수
